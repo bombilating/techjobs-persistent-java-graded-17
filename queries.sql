dd18575e8d0b80689771474f0d76570fcec9fccd
Jeremy <thisismejeremy@gmail.com>
2024-06-17 22:23:56 -0400

    Graded Assignment 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 --Part 1
 
+--SQL TASK: At this point, you will have one table, job.
+--In queries.sql under “Part 1”, list the columns and their data types in the table as a SQL comment.
+
+-- Columns in the job table:
+-- id: int, not null, primary key
+-- employer: varchar(255), nullable
+-- name: varchar(255), nullable
+-- skills: varchar(255), nullable
+
 --Part 2
+
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
+DROP TABLE job;
+
 --Part 4
+
+SELECT * FROM skill LEFT JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -8,11 +8,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @Size(max = 100)
+    @NotNull(message = "cannot be blank")
     private String name;
 
     public int getId() {
